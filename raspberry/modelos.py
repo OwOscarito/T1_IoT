de1a3a7799cea9abeb0174d657d56395962825e6
Lucas Llort <lucasllort@gmail.com>
2025-05-03 15:15:23 -0400

    server como servicio de docker

--- raspberry/modelos.py
+++ raspberry/modelos.py
@@ -1,22 +1,46 @@
-from peewee import Model, PostgresqlDatabase
+import os
+import typing
+from peewee import Model, PostgresqlDatabase, TimestampField, IntegerField, CharField, FloatField, CompositeKey
 
-# Configuración de la base de datos
-db_config = {
-    'host': 'localhost', 
-    'port': 5432, 
-    'user': 'postgres', 
-    'password': 'postgres', 
-    'database': 'db'
-}
-db = PostgresqlDatabase(**db_config)
+POSTGRES_DB = os.getenv('POSTGRES_DB', 'postgres')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
+POSTGRES_PORT = int(os.getenv('POSTGRES_PORT', '5432'))
+POSTGRES_USER = os.getenv('POSTGRES_USER', 'postgres')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'postgres')
 
+db = PostgresqlDatabase(
+    database = POSTGRES_DB,
+    host = POSTGRES_HOST,
+    port = POSTGRES_PORT,
+    user = POSTGRES_USER,
+    password = POSTGRES_PASSWORD
+)
+_ = db.connect()
+    
 # Definición de un modelo
 class BaseModel(Model):
     class Meta:
-        database = db
+        database: PostgresqlDatabase = db
 
 # Ahora puedes definir tus modelos específicos heredando de BaseModel
 # y db estará conectado al servicio de PostgreSQL cuando realices operaciones de base de datos.
 
+# Ver la documentación de peewee para más información, es super parecido a Django
+# https://docs.peewee-orm.com/en/latest/peewee/quickstart.html
 
-## Ver la documentación de peewee para más información, es super parecido a Django
+@typing.final
+class Data(Model):
+    type = CharField()
+    value = FloatField()
+    timestamp = TimestampField()
+    device_id = IntegerField()
+    mac_address = CharField()
+
+    @typing.final
+    class Meta():
+        primary_key = CompositeKey('device_id', 'timestamp')
+        database: PostgresqlDatabase = db
+
+
+db.create_tables([Data])
+
--- raspberry/server.py
+++ raspberry/server.py
@@ -1,21 +1,23 @@
 import socket
+import os
+import sys
 
 HOST = '0.0.0.0'  # Escucha en todas las interfaces disponibles
-PORT = 1234       # Puerto en el que se escucha
+PORT = int(os.getenv('SERVER_PORT', '1234'))
 
 # Crea un socket para IPv4 y conexión TCP
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
 
-    print("El servidor está esperando conexiones en el puerto", PORT)
+    print(f"El servidor está esperando conexiones en el puerto {PORT}", file=sys.stderr)
 
     while True:
         conn, addr = s.accept()  # Espera una conexión
         with conn:
-            print('Conectado por', addr)
+            print(f"Conección recibida de {addr}", file=sys.stderr)
             data = conn.recv(1024)  # Recibe hasta 1024 bytes del cliente
             if data:
-                print("Recibido: ", data.decode('utf-8'))
+                print(f"Mensaje: {data.decode('utf-8')}", end='', file=sys.stderr)
                 respuesta = "tu mensaje es: " + data.decode('utf-8')
                 conn.sendall(respuesta.encode('utf-8'))  # Envía la respuesta al cliente
